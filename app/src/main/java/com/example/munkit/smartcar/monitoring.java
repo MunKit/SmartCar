45312e62227b02fde4383efc05a31bbcb0c8c20c
MunKit <ngmk93@gmail.com>
2017-03-02 22:33:16 +0800

    refactoring code

--- app/src/main/java/com/example/munkit/smartcar/monitoring.java
+++ app/src/main/java/com/example/munkit/smartcar/monitoring.java
@@ -6,12 +6,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import android.support.v7.app.NotificationCompat;
-import android.view.View;
 import android.view.WindowManager;
-import android.widget.Button;
 import android.content.Intent;
-import android.widget.EditText;
-import android.widget.TextView;
 import android.widget.SeekBar;
 import android.widget.Toast;
 
